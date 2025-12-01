7171f56fa2e92db652cf3b933d2d12787d83cfd5
@nxora <omotoyedave4@gmail.com>
2025-12-01 15:48:11 +0100

    Editted Facts.get() to support Map and List

--- api/src/main/java/dave/rulesengine/Facts.java
+++ api/src/main/java/dave/rulesengine/Facts.java
@@ -19,15 +19,26 @@ public class Facts {
         Object current = data;
         for (String part : parts) {
 
-            if (!(current instanceof Map<?, ?> currentMap) || !(current instanceof List<?> currentList)) {
+            if (current instanceof Map) {
+                current = ((Map<?, ?>) current).get(part);
                  return null;
+            } else if (current instanceof List<?>) {
+                int index;
+
+                try{
+                    index = Integer.parseInt(part);
+                } catch (NumberFormatException e){
+                    return null;
+                }
+                List<?> list = (List<?>) current;
+                if (index < 0 || index >= list.size()){
+                    return null;
+                }
+                current = list.get(index);
+            }else {
+                    return null;
             }
-            if (current instanceof Map){
-                current = currentMap.get(part);
-            }
-            if (current instanceof List){
-                current = currentList.get(Integer.parseInt(part));
-            }
+
             if (current == null) {
                 return null;
             }
