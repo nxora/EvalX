5b7573a19ea2b9628544921fd6f9dd0ad6831cda
@nxora <omotoyedave4@gmail.com>
2025-12-02 14:31:44 +0100

    Upated Eval context

--- api/src/main/java/dave/rulesengine/EvalContext.java
+++ api/src/main/java/dave/rulesengine/EvalContext.java
@@ -1,14 +1,27 @@
 package dave.rulesengine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EvalContext {
     private final Facts facts;
+    private final Map<String, Object> variables = new HashMap<>();
 
     public EvalContext(Facts facts) {
         this.facts = facts;
     }
 
-    public Object getFact(String key){return facts.get(key);}
-    public Facts getfact (){
+    public Object get(String key){
+        if (variables.containsKey(key)) {
+            return variables.get(key);
+        }
+        return facts.get(key);
+    }
+
+    public void set(String key, Object value){
+        variables.put(key, value);
+    }
+    public Facts getFacts (){
         return facts;
     }
 
